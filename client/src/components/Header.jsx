f1e8b3b29b68c91ea5c57f4a918b1362912c096d
Aliasgar <jhalodwalaaliasgar53@gmail.com>
2024-02-13 13:01:55 +0530

    Header completed

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,132 @@
+import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+
+function Header() {
+  return (
+    <Box
+      width="100vw"
+      bgcolor="#ebf8ff"
+      height={70}
+      display="flex"
+      alignItems="center"
+      justifyContent="space-between"
+      p={2}
+    >
+      <Stack direction="row" gap="10px">
+        <svg
+          height="32px"
+          viewBox="0 -0.5 25 25"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+          <g
+            id="SVGRepo_tracerCarrier"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></g>
+          <g id="SVGRepo_iconCarrier">
+            {" "}
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M7.30524 15.7137C6.4404 14.8306 5.85381 13.7131 5.61824 12.4997C5.38072 11.2829 5.50269 10.0233 5.96924 8.87469C6.43181 7.73253 7.22153 6.75251 8.23924 6.05769C10.3041 4.64744 13.0224 4.64744 15.0872 6.05769C16.105 6.75251 16.8947 7.73253 17.3572 8.87469C17.8238 10.0233 17.9458 11.2829 17.7082 12.4997C17.4727 13.7131 16.8861 14.8306 16.0212 15.7137C14.8759 16.889 13.3044 17.5519 11.6632 17.5519C10.0221 17.5519 8.45059 16.889 7.30524 15.7137V15.7137Z"
+              stroke="#262626"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            ></path>{" "}
+            <path
+              d="M11.6702 7.20292C11.2583 7.24656 10.9598 7.61586 11.0034 8.02777C11.0471 8.43968 11.4164 8.73821 11.8283 8.69457L11.6702 7.20292ZM13.5216 9.69213C13.6831 10.0736 14.1232 10.2519 14.5047 10.0904C14.8861 9.92892 15.0644 9.4888 14.9029 9.10736L13.5216 9.69213ZM16.6421 15.0869C16.349 14.7943 15.8741 14.7947 15.5815 15.0879C15.2888 15.381 15.2893 15.8559 15.5824 16.1485L16.6421 15.0869ZM18.9704 19.5305C19.2636 19.8232 19.7384 19.8228 20.0311 19.5296C20.3237 19.2364 20.3233 18.7616 20.0301 18.4689L18.9704 19.5305ZM11.8283 8.69457C12.5508 8.61801 13.2384 9.02306 13.5216 9.69213L14.9029 9.10736C14.3622 7.83005 13.0496 7.05676 11.6702 7.20292L11.8283 8.69457ZM15.5824 16.1485L18.9704 19.5305L20.0301 18.4689L16.6421 15.0869L15.5824 16.1485Z"
+              fill="#262626"
+            ></path>{" "}
+          </g>
+        </svg>
+        <input
+          style={{
+            backgroundColor: "transparent",
+            border: "none",
+            outline: "none",
+            width: "300px",
+          }}
+          type="text"
+          placeholder="Search for students or classes"
+        />
+      </Stack>
+      <Stack direction="row" alignItems="center" gap={1.5}>
+        <svg
+          width="24px"
+          height="24px"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          cursor="pointer"
+        >
+          <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+          <g
+            id="SVGRepo_tracerCarrier"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></g>
+          <g id="SVGRepo_iconCarrier">
+            {" "}
+            <path
+              d="M18.7491 9.70957V9.00497C18.7491 5.13623 15.7274 2 12 2C8.27256 2 5.25087 5.13623 5.25087 9.00497V9.70957C5.25087 10.5552 5.00972 11.3818 4.5578 12.0854L3.45036 13.8095C2.43882 15.3843 3.21105 17.5249 4.97036 18.0229C9.57274 19.3257 14.4273 19.3257 19.0296 18.0229C20.789 17.5249 21.5612 15.3843 20.5496 13.8095L19.4422 12.0854C18.9903 11.3818 18.7491 10.5552 18.7491 9.70957Z"
+              stroke="#262626"
+              strokeWidth="1.5"
+            ></path>{" "}
+            <path
+              d="M7.5 19C8.15503 20.7478 9.92246 22 12 22C14.0775 22 15.845 20.7478 16.5 19"
+              stroke="#262626"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+            ></path>{" "}
+          </g>
+        </svg>
+        <Stack direction="row" alignItems="center" gap={0.5}>
+          <img
+            src="https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fHww"
+            alt="Profile"
+            height="48px"
+            width="48px"
+            style={{ borderRadius: "50%", objectFit: "cover" }}
+          />
+          <Stack>
+            <Typography fontSize="12px" fontWeight={600}>
+              Caroline Stokes
+            </Typography>
+            <Typography fontSize="12px" color="#083d18" fontWeight={700}>
+              Administrator
+            </Typography>
+          </Stack>
+          <svg
+            width="28px"
+            height="28px"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+            <g
+              id="SVGRepo_tracerCarrier"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+              {" "}
+              <path
+                d="M7 10L12 15L17 10"
+                stroke="#262626"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              ></path>{" "}
+            </g>
+          </svg>
+        </Stack>
+      </Stack>
+    </Box>
+  );
+}
+
+export default Header;
